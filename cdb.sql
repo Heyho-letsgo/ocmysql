01a480589751520a217c6d025aaffceae170d0fc
Baudin-Laurencin <abaudin@gmail.com>
2017-09-18 08:15:01 +0000

    Rajout de différentes tables et mises en majuscules

--- cdb.sql
+++ cdb.sql
@@ -1,16 +1,12 @@
-USE C9
+DROP TABLE IF EXISTS ANIMAL;
 
-
-
-DROP TABLE IF EXISTS Animal;
-
-CREATE TABLE IF NOT EXISTS Animal (
-    id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT, 
-    espece VARCHAR(40) NOT NULL,
-    sexe VARCHAR(40),
-    dateNaissance DATETIME NOT NULL,
-    nom VARCHAR(30),
-    commentaires TEXT,
+CREATE TABLE IF NOT EXISTS ANIMAL (
+    ID SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT, 
+    ANI_Espece VARCHAR(40) NOT NULL,
+    ANI_Sexe VARCHAR(40),
+    ANI_DateNaissance DATETIME NOT NULL,
+    ANI_Nom VARCHAR(30),
+    ANI_Commentaires TEXT,
     PRIMARY KEY(id)
     )
     ENGINE = INNODB;
@@ -39,7 +35,7 @@ CREATE TABLE CommTel(
     
 SHOW TABLES;
 
-EXPLAIN Animal;
+EXPLAIN ANIMAL;
 EXPLAIN Person;
 EXPLAIN CommTel;
 
@@ -58,5 +54,60 @@ INTO TABLE Person
 FIELDS TERMINATED BY ';' ENCLOSED BY ''
 LINES TERMINATED BY ''(nom,numTel,mail);
 
+create table if not exists LIBRARY (
+id smallint unsigned not null auto_increment, 
+LIB_Name varchar(40),
+primary key(id)
+) 
+engine=innodb;
+
+EXPLAIN LIBRARY;
 
 
+DROP TABLE IF EXISTS EMPLOYE;
+CREATE TABLE IF NOT EXISTS EMPLOYE (
+    ID smallint unsigned not null auto_increment, 
+    EMP_LastName varchar (40), 
+    EMP_FirstName varchar (40),
+    PRIMARY KEY(ID)
+    ) 
+engine = innodb;
+
+EXPLAIN EMPLOYE;
+
+INSERT INTO 
+EMPLOYE (EMP_LastName, EMP_FirstName) 
+VALUES('Baudin-Laurencin','Charlotte'), ('Baudin-Laurencin', 'André'), ('David','Niven');
+
+SELECT * FROM EMPLOYE;
+
+SELECT * FROM EMPLOYE WHERE (EMP_FirstName = 'André') OR (EMP_LastName = 'Baudin-Laurencin');
+
+
+DROP TABLE IF EXISTS CHAMBRE;
+CREATE TABLE IF NOT EXISTS CHAMBRE (
+    ID SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    CHA_TYPE VARCHAR (7),
+    CHA_NOMBRE TINYINT UNSIGNED,
+    PRIMARY KEY(ID)
+)
+ENGINE = INNODB;
+
+EXPLAIN CHAMBRE;
+
+
+INSERT INTO 
+CHAMBRE (CHA_TYPE, CHA_NOMBRE)
+VALUES 
+    ('Chambre',1),
+    ('Chambre',2),
+    ('Chambre',3),
+    ('Chambre',4),
+    ('Chambre',5),
+    ('Chambre',6),
+    ('Chambre',7),
+    ('Chambre',8);
+SELECT * FROM CHAMBRE;
+
+
+SELECT * FROM CHAMBRE WHERE (CHA_NOMBRE = 7) OR (CHA_NOMBRE = 6) ORDER BY CHA_NOMBRE DESC; 
